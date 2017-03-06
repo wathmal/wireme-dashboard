73e4d732dde267400f544f3f92ada2cccef756e7
Sasitha Sonnadara <wathmal@hotmail.com>
2017-03-06 12:17:40 +0530

    added metatags on server side.

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,7 @@
 /**
  * Created by wathmal on 11/30/16.
  */
+import fs from 'fs';
 import path from 'path';
 import express from 'express';
 import bodyParser from 'body-parser';
@@ -9,7 +10,7 @@ import webpack from 'webpack';
 // import webpackHotMiddleware from 'webpack-hot-middleware';
 // import config from './../webpack.config.js';
 import Api from './api';
-
+import OG from './services/OpenGraph';
 
 const app = express();
 
@@ -28,7 +29,34 @@ console.log("node env: "+ process.env.NODE_ENV);
 app.use('/api', Api );
 app.get('*', function response(req, res) {
 
-    res.sendFile(path.join(__dirname, 'index.html'));
+    const reqPath = req.path;
+    let metaHTML="";
+    switch (reqPath) {
+        case '/':
+
+            let og= new OG("wireme iot platform", "http://wireme.projects.mrt.ac.lk/images/landing/nemesis.jpg", "a RIDICULOUSLY easy IoT platform with zero coding");
+            metaHTML = og.generateMeta();
+            break;
+        case '/dashboard':
+            let og= new OG("wireme - dashboard", "http://wireme.projects.mrt.ac.lk/images/landing/dashboard-mac.png", "monitor, control your wireme gadgets from anywhere in the world");
+            metaHTML = og.generateMeta();
+            break;
+
+        case '/quickstart':
+            let og= new OG("wireme - quick start", "http://wireme.projects.mrt.ac.lk/images/tutorial/quickstart/dashboard_on_tab.jpg", "quick start guide for wireme IoT platform");
+            metaHTML = og.generateMeta();
+            break;
+    }
+
+    fs.readFile(path.join(__dirname, 'index.html'), 'utf8', function (err, file) {
+        if (err) {
+            return console.log(err);
+        }
+        const document = file.replace(/<meta \/>/, metaHTML);
+        res.send(document);
+    });
+
+    // res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 const port= process.env.PORT || 3000;
---
+++ src/server/services/OpenGraph.js
@@ -0,0 +1,30 @@
+/**
+ * Created by wathmal on 3/6/17.
+ */
+
+let og= {};
+class OpenGraph{
+
+    constructor(title, image, description){
+        og.title= title;
+        og.image= image;
+        og.description= description;
+    }
+
+    generateMeta(){
+
+        let html ="";
+
+        for (var key in og) {
+            if (og.hasOwnProperty(key)) {
+                //console.log(key + " -> " + p[key]);
+                //    <meta property="og:title" content="wireme iot platform" />
+                html += `<meta property="og:${key}" content="${og[key]}"> \n`;
+            }
+        }
+
+        return html;
+    }
+}
+
+export default OpenGraph;
